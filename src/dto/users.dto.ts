74987c1b9284b947a7cbb9daf6f685b22b4b1646
med363 <blibechmedamine@gmail.com>
2023-06-14 06:10:31 +0100

    add new package dto

---
+++ src/dto/users.dto.ts
@@ -0,0 +1,12 @@
+import { IsEmail, IsNotEmpty } from "class-validator";
+
+export class UserDto{
+    @IsNotEmpty()
+    fullname: string;
+    @IsEmail()
+    email: string;
+    @IsNotEmpty()
+    age: number;
+    @IsNotEmpty()
+    country: string;
+}
