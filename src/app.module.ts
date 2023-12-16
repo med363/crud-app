54f95f8ddc2fbba399a3d60f0658a213eb730ebb
med363 <Vous@exemple.com>
2023-12-16 12:44:53 +0100

    correct uri

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,6 @@ import { UsersModule } from './users/users.module';
 import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb+srv://crud:crud@cluster0.aiwpfnf.mongodb.net/?retryWrites=true&w=majority'),UsersModule],
+  imports: [MongooseModule.forRoot('mongodb+srv://crud:crud@cluster0.bkg61.mongodb.net/?retryWrites=true&w=majority'),UsersModule],
  })
 export class AppModule {}
