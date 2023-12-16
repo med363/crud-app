22ecde38dbb8358803e862059a45d6d8e89960c5
med363 <Vous@exemple.com>
2023-12-16 13:40:01 +0100

    connect atlas

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,9 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { MongooseModule } from '@nestjs/mongoose';
-import { MysqlModule } from './mysql/mysql.module';
-import { MysqlConnectionService } from './mysql-connection/mysql-connection.service';
-import { UsersMysqlService } from './users-mysql/users-mysql.service';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb+srv://users:gwbjFdj3uQnH1hui@cluster0.bkg61.mongodb.net/?retryWrites=true&w=majority'),UsersModule, MysqlModule],
-  providers: [MysqlConnectionService, UsersMysqlService],
+  imports: [MongooseModule.forRoot('mongodb+srv://blibechmohamedamine:crud@cluster0.n5gl1fo.mongodb.net/?retryWrites=true&w=majority'),UsersModule],
+  providers: [],
  })
 export class AppModule {}
--- src/mysql-user/mysql-user.spec.ts
+++
@@ -1,7 +0,0 @@
-import { MysqlUser } from './mysql-user';
-
-describe('MysqlUser', () => {
-  it('should be defined', () => {
-    expect(new MysqlUser()).toBeDefined();
-  });
-});
